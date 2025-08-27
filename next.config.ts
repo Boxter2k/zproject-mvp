87a1cc733d214a16b1d212a5034bff466e0bd3a5
Boxter2k <fg846816@gmail.com>
2025-08-27 12:13:25 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/service-offer/:path*',
+        destination: 'https://myzproject.com/service-offer/:path*',
+      },
+      {
+        source: '/posts/:path*',
+        destination: 'https://myzproject.com/posts/:path*',
+      }
+    ];
+  },
 };
 
 export default nextConfig;
